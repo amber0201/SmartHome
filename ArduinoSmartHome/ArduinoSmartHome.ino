287788b0fa77b781ea632712038de3f50caab323
amber0201 <s810653@email.nlhs,tyc,edu,tw>
2020-09-28 15:51:53 +0800

    增加蜂鳴器發聲功能

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,14 +1,16 @@
 void setup() {
- pinMode(12,OUTPUT);
- pinMode(11,OUTPUT);
- 
+ pinMode(12, OUTPUT);
+ pinMode(11, OUTPUT);
+ pinMode(7, OUTPUT);
 }
 
 void loop() {
 digitalWrite(12, HIGH);
 pinMode(11,LOW);
+tone(7, 100, 500);
 delay(500);
 digitalWrite(12, LOW);
-pinMode(11,HIGH);
+pinMode(11, HIGH);
+tone(7, 800, 500);
 delay(500);
 }
