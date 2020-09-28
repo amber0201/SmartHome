4b5b150d058fdad0629a879523fe0f0fe1bf88e0
amber0201 <s810653@email.nlhs,tyc,edu,tw>
2020-09-28 15:48:53 +0800

    完成一顆0.5秒閃一次的警示燈

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,10 +1,14 @@
 void setup() {
  pinMode(12,OUTPUT);
+ pinMode(11,OUTPUT);
+ 
 }
 
 void loop() {
 digitalWrite(12, HIGH);
+pinMode(11,LOW);
 delay(500);
 digitalWrite(12, LOW);
+pinMode(11,HIGH);
 delay(500);
 }
